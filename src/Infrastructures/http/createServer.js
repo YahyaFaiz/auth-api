356f333b48f863886920c030790043d8fd0ed363
yahyafaiz <faiz.diennur.yahya.com>
2024-12-22 08:17:42 +0700

    Deskripsi perubahan

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -6,60 +6,60 @@ const authentications = require('../../Interfaces/http/api/authentications');
 const config = require('../../Commons/config');
 
 const createServer = async (container) => {
-  const server = Hapi.server({
-    port: config.app.port,
-    host: config.app.host,
-    debug: config.app.debug,
-  });
+    const server = Hapi.server({
+        port: process.env.PORT,
+        host: process.env.HOST,
+        debug: config.app.debug,
+    });
 
-  await server.register([
-    {
-      plugin: users,
-      options: { container },
-    },
-    {
-      plugin: authentications,
-      options: { container },
-    },
-  ]);
+    await server.register([
+        {
+            plugin: users,
+            options: { container },
+        },
+        {
+            plugin: authentications,
+            options: { container },
+        },
+    ]);
 
-  server.ext('onPreResponse', (request, h) => {
-    // mendapatkan konteks response dari request
-    const { response } = request;
+    server.ext('onPreResponse', (request, h) => {
+        // mendapatkan konteks response dari request
+        const { response } = request;
 
-    if (response instanceof Error) {
-      // bila response tersebut error, tangani sesuai kebutuhan
-      const translatedError = DomainErrorTranslator.translate(response);
+        if (response instanceof Error) {
+            // bila response tersebut error, tangani sesuai kebutuhan
+            const translatedError = DomainErrorTranslator.translate(response);
 
-      // penanganan client error secara internal.
-      if (translatedError instanceof ClientError) {
-        const newResponse = h.response({
-          status: 'fail',
-          message: translatedError.message,
-        });
-        newResponse.code(translatedError.statusCode);
-        return newResponse;
-      }
+            // penanganan client error secara internal.
+            if (translatedError instanceof ClientError) {
+                const newResponse = h.response({
+                    status: 'fail',
+                    message: translatedError.message,
+                });
+                newResponse.code(translatedError.statusCode);
+                return newResponse;
+            }
 
-      // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
-      if (!translatedError.isServer) {
+            // mempertahankan penanganan client error oleh hapi secara native, seperti 404, etc.
+            if (!translatedError.isServer) {
+                return h.continue;
+            }
+
+            // penanganan server error sesuai kebutuhan
+            const newResponse = h.response({
+                status: 'error',
+                message: 'terjadi kegagalan pada server kami',
+            });
+            newResponse.code(500);
+            return newResponse;
+        }
+
+        // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
         return h.continue;
-      }
+    });
 
-      // penanganan server error sesuai kebutuhan
-      const newResponse = h.response({
-        status: 'error',
-        message: 'terjadi kegagalan pada server kami',
-      });
-      newResponse.code(500);
-      return newResponse;
-    }
-
-    // jika bukan error, lanjutkan dengan response sebelumnya (tanpa terintervensi)
-    return h.continue;
-  });
-
-  return server;
+    return server;
 };
 
 module.exports = createServer;
